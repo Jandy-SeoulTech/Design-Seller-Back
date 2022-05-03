ee6c9b11b1d5d27b9d1f1f21e068a691ad13045c
EomYoosang <dbtkd1801@gmail.com>
2022-05-03 09:33:16 +0900

    Initial commit

---
+++ src/main/java/jandy3/DesignSeller/DesignSellerApplication.java
@@ -0,0 +1,13 @@
+package jandy3.DesignSeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignSellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jandy3/DesignSeller/DesignSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package jandy3.DesignSeller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
