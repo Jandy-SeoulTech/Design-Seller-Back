8c54f9244d2bf5439fae2833aee841f5a3e2a12e
EomYoosang <dbtkd1801@gmail.com>
2022-05-14 03:32:48 +0900

    feat: access management

--- src/main/java/jandy3/DesignSeller/config/SecurityConfig.java
+++ src/main/java/jandy3/DesignSeller/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package jandy3.DesignSeller.config;
 
+import jandy3.DesignSeller.config.oauth.PrincipalOauth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +17,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // 특정 주소 접근시 권한 및 인증을 위한 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    private PrincipalOauth2UserService principalOauth2UserService;
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean
     public BCryptPasswordEncoder encodePwd() {
@@ -26,8 +27,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.cors();
         http.csrf().disable();
+//        http.authorizeRequests()
+//                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+//                .antMatchers("/api/v1/**").permitAll()
+//                .and()
+//                .formLogin().disable();
         http.authorizeRequests()
 //                .antMatchers("/user/**").authenticated()
                 //.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
@@ -36,13 +42,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/login");
-//                .loginProcessingUrl("/loginProc")
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/loginProc")
 //                .defaultSuccessUrl("/")
-//                .and()
-//                .oauth2Login()
-//                .loginPage("/login");
-//                .userInfoEndpoint()
-//                .userService(principalOauth2UserService);
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
     }
 }
