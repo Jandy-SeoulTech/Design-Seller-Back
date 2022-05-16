ddc84a42d3f9d94dac429b79d5c44c92a421b490
EomYoosang <dbtkd1801@gmail.com>
2022-05-16 18:55:19 +0900

    feat: get user info

--- src/main/java/jandy3/DesignSeller/controller/UserController.java
+++ src/main/java/jandy3/DesignSeller/controller/UserController.java
@@ -1,20 +1,17 @@
 package jandy3.DesignSeller.controller;
 
-import jandy3.DesignSeller.model.User;
+import jandy3.DesignSeller.config.auth.PrincipalDetails;
+import jandy3.DesignSeller.config.oauth.annotation.CurrentUser;
+import jandy3.DesignSeller.config.oauth.exception.ResourceNotFoundException;
+import jandy3.DesignSeller.domain.User;
 import jandy3.DesignSeller.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpHeaders;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,9 +19,12 @@ public class UserController {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping(value = "/user")
-    public String user() {
-        return "user";
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public String user(@CurrentUser PrincipalDetails principalDetails) {
+        return String.valueOf(userRepository.findById(principalDetails.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", principalDetails.getId())));
     }
 
     @PostMapping(value = "/join")
@@ -37,32 +37,5 @@ public class UserController {
         userRepository.save(user);
 
         return user.getId();
-    }
-
-    @GetMapping("/auth/kakao/callback")
-    public @ResponseBody String kakaoCallBack(String code) {
-
-//        RestTemplate rt = new RestTemplate();
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Contents-type", "application/x-www-form-urlencoded;charset=utf-8");
-//
-//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//        params.add("grant_type", "authorization_code");
-//        params.add("client_id", "f48783f49d03bef07c4c21b53e293db9");
-//        params.add("client_secret", "g7mjZblRpHqyt9nhsFL6PoHu4kOWhwvm");
-//        params.add("redirect_uri","http://localhost:8080/login/oauth2/code/kakao");
-//        params.add("code", code);
-//
-//        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
-//
-//        ResponseEntity response = rt.exchange(
-//                "https://kauth.kakao.com/oauth/token",
-//                HttpMethod.POST,
-//                kakaoTokenRequest,
-//                String.class
-//        );
-//        System.out.println("response: " + response);
-//        return "response: " + response;
-        return "redirect:/auth?code="+"12341234";
     }
 }
--- src/main/java/jandy3/DesignSeller/repository/UserRepository.java
+++ src/main/java/jandy3/DesignSeller/repository/UserRepository.java
@@ -1,8 +1,7 @@
 package jandy3.DesignSeller.repository;
 
-import jandy3.DesignSeller.model.User;
+import jandy3.DesignSeller.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
