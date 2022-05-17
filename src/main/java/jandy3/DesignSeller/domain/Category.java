a97f6e512bd3ce3acb5fcfae9f632ebe2c676f3c
EomYoosang <dbtkd1801@gmail.com>
2022-05-17 09:11:05 +0900

    feat: Add domains

---
+++ src/main/java/jandy3/DesignSeller/domain/Category.java
@@ -0,0 +1,37 @@
+package jandy3.DesignSeller.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<CategoryItem> items = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    //==연관관계 편의 메서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+}
