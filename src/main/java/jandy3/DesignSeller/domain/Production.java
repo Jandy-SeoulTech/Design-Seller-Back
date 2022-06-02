8dde91a59f7781b550dc80731d07979b9ec6fc82
EomYoosang <dbtkd1801@gmail.com>
2022-06-02 18:09:41 +0900

    fix: Fix JPA error by adding @JsonIgnore annotation to Production domain
    프로덕션 엔티티에서 다대일 양방향 연관관계로 설정한 Category객체를 FetchType.LAZY로 바꾸면서 Json객체 반환 시 Production 객체에 category가 비게됨. category를 JsonIgnore 처리함으로써 해당 객체가 비어도 무관하게 만들어 해결

--- src/main/java/jandy3/DesignSeller/domain/Production.java
+++ src/main/java/jandy3/DesignSeller/domain/Production.java
@@ -1,5 +1,6 @@
 package jandy3.DesignSeller.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -25,6 +26,7 @@ public class Production {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
+    @JsonIgnore
     private Category category;
 
     @CreationTimestamp
