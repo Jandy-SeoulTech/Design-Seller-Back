6ff7e99918c6d5c0e12a4091b76d8fdebf63d674
EomYoosang <dbtkd1801@gmail.com>
2022-06-14 23:56:36 +0900

    feature(request): Change Dto about Production Request
    Request 기능 관련 dto 수정 및 RequestApiController 내부에 정의
    이전 dto클래스 삭제

--- src/main/java/jandy3/DesignSeller/api/RequestApiController.java
+++ src/main/java/jandy3/DesignSeller/api/RequestApiController.java
@@ -1,27 +1,46 @@
 package jandy3.DesignSeller.api;
 
 import jandy3.DesignSeller.auth.PrincipalDetails;
-import jandy3.DesignSeller.dto.IdResponse;
-import jandy3.DesignSeller.dto.RequestInfo;
 import jandy3.DesignSeller.auth.annotation.CurrentUser;
+import jandy3.DesignSeller.dto.ProductionOptionInfo;
 import jandy3.DesignSeller.service.RequestService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class RequestApiController {
     private final RequestService requestService;
 
     @PostMapping(value = "/request/new")
-    public IdResponse createRequest(
+    public CreateRequestResponse createRequest(
             @CurrentUser PrincipalDetails principalDetails,
-            @RequestBody RequestInfo requestInfo) {
+            @RequestBody CreateRequestInfo createRequestInfo) {
         Long requestId = requestService
-                .createRequest(principalDetails.getId(), requestInfo.getOptions(), requestInfo.getRequestFiles());
-        return new IdResponse(true, requestId);
+                .createRequest(principalDetails.getId(), createRequestInfo.getOptions(), createRequestInfo.getRequestFiles());
+        return new CreateRequestResponse(requestId);
     }
 
+    @Data
+    @AllArgsConstructor
+    static class CreateRequestResponse {
+        private Long id;
+    }
+
+    @Data
+    @Getter
+    static class CreateRequestInfo {
+        private Long productionId;
+        private List<ProductionOptionInfo> options;
+        private List<String> requestFiles;
+    }
 }
