b296deb7390d9361299481fec129a3084e90b52b
EomYoosang <dbtkd1801@gmail.com>
2022-06-18 21:44:51 +0900

    docs(swagger): Add swagger documentation
    PrincipalDetails, Pageable 파라미터 클래스 타입 변환을 위해 SwaggerConfig 수정

--- src/main/java/jandy3/DesignSeller/config/SwaggerConfig.java
+++ src/main/java/jandy3/DesignSeller/config/SwaggerConfig.java
@@ -1,21 +1,31 @@
 package jandy3.DesignSeller.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jandy3.DesignSeller.auth.PrincipalDetails;
+import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig implements WebMvcConfigurer {
 
+    TypeResolver typeResolver = new TypeResolver();
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) { //spring-security와 연결할 때 이 부분을 작성하지 않으면 404에러가 뜬다.
         registry.addResourceHandler("swagger-ui/")
@@ -26,7 +36,13 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api() { //swagger를 연결하기 위한 Bean 작성
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
+                .alternateTypeRules(
+                        AlternateTypeRules.newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class))
+                )
+                .alternateTypeRules(
+                        AlternateTypeRules.newRule(typeResolver.resolve(PrincipalDetails.class), typeResolver.resolve(UserToken.class))
+                )
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -36,9 +52,27 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     private ApiInfo apiInfo() { //선택
         return new ApiInfoBuilder()
-                .title("Omoolen Release") //자신에게 맞는 타이틀을 작성해준다.
+                .title("m2m develop") //자신에게 맞는 타이틀을 작성해준다.
                 .description("backend api document") //알맞는 description을 작성해준다.
-                .version("0.0") //알맞는 버전을 작성해준다.
+                .version("0.1") //알맞는 버전을 작성해준다.
                 .build();
+    }
+
+    @Data
+    static class UserToken {
+        @ApiModelProperty(value = "Authorization: Bearer {token}")
+        private String token;
+    }
+
+    @Data
+    static class Page {
+        @ApiModelProperty(value = "페이지 번호(0..N)")
+        private Integer page;
+
+        @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]")
+        private Integer size;
+
+        @ApiModelProperty(value = "정렬(사용법: view|like|createDate,ASC|DESC)")
+        private String sort;
     }
 }
